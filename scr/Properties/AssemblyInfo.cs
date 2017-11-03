349d9af361b5fcc20073d5fee96d16890dc8a780
alexguirre <alexguirre.aa@gmail.com>
2017-11-03 06:41:50 +0100

    Changes for AppVeyor automated builds

--- scr/Properties/AssemblyInfo.cs
+++ scr/Properties/AssemblyInfo.cs
@@ -4,13 +4,19 @@ using Rage.Attributes;
 
 [assembly: AssemblyTitle("Vehicle Gadgets+")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyCompany("alexguirre")]
 [assembly: AssemblyProduct("Vehicle Gadgets+")]
-[assembly: AssemblyCopyright("Copyright ©  2017 alexguirre")]
+[assembly: AssemblyCopyright("Copyright ©  $CR_YEAR$ alexguirre")] // set by AppVeyor
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("32b66d4a-850b-455b-a1c2-fb0a2d221218")]
-[assembly: AssemblyVersion("0.2.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")] // set by AppVeyor
+[assembly: AssemblyFileVersion("0.0.0.0")] // set by AppVeyor
+[assembly: AssemblyInformationalVersion("0.0")] // set by AppVeyor
 [assembly: Plugin("Vehicle Gadgets+", Author = "alexguirre", PrefersSingleInstance = true)]
